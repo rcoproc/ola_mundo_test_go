28082268b41c09c6d62aafb3e20ac0790f47b8bd
Ricardo Oliveira <rioliveira@bionexo.com>
2020-02-08 18:42:03 -0300

    Change default value in metodh Ola()

--- ola_test.go
+++ ola_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-    resultado := Ola("Ricardo")
-    esperado := "Olá, Ricardo"
-
-    if resultado != esperado {
-        t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+    verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+        t.Helper()
+        if resultado != esperado {
+            t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+        }
     }
-}
+
+    t.Run("diz olá para as pessoas", func(t *testing.T) {
+        resultado := Ola("Ricardo")
+        esperado := "Olá, Ricardo"
+        verificaMensagemCorreta(t, resultado, esperado)
+    })
+
+    t.Run("'Mundo' como padrão para 'string' vazia", func(t *testing.T) {
+        resultado := Ola("")
+        esperado := "Olá, Mundo"
+        verificaMensagemCorreta(t, resultado, esperado)
+    })
+  }
